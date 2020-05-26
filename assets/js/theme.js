2823bd9d9afb04778119e793713a5d87432203ca
Tawhidul Islam Khan <tawhid.developer@gmail.com>
2020-05-26 23:46:51 +0600

    swiper js and preloader added

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,27 +1,87 @@
-"use strict";
+(function($) {
+  
+  "use strict";
 
+  $(window).on('load', function() { 
+    $('.preloader').delay(350).fadeOut('slow'); 
+  }); 
+  
 $(".btn-play").magnificPopup({
   type: "iframe",
 });
 
-tns({
-  container: ".portfolio-slider",
-  items: 5,
-  autoplay: true,
-  gutter: 20,
-  touch: true,
-  controls: false,
-  responsive: {
-    640: {
-      edgePadding: 20,
-      gutter: 20,
-      items: 2,
-    },
-    700: {
-      gutter: 30,
-    },
-    900: {
-      items: 5,
-    },
+function counterInit() {
+  document.querySelectorAll('.counter').forEach(el => {
+    let start = 0;
+    let end = el.dataset.number
+    let suffix = el.dataset.suffix || ''
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(9000 / range));
+    var timer = setInterval(function() {
+      current += increment;
+      el.innerHTML = current + suffix;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+  
+  });
+}
+
+counterInit();
+
+})(jQuery);
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    }
+  }
+});
+
+new Swiper('.testimonials', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  }
 });
